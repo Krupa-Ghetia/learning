12ccac31b35dbfa66c34f5ca2db0f53bf50aafa6
Krupa-Ghetia <krips2407@gmail.com>
2022-07-28 03:33:49 +0530

    solve sort the array problem 1

---
+++ arrays/sort_the_array_1/README.md
@@ -0,0 +1,4 @@
+Given an array of n integers, the first n-1 integers are sorted.  
+Sort the entire array.
+
+Example: A = [1, 2, 3, 6, 8, 15, 20, 14]
---
+++ arrays/sort_the_array_1/sort_the_array.py
@@ -0,0 +1,19 @@
+def sort_the_array(arr):
+    element_to_move = arr[len(arr) - 1]
+
+    for i in range(len(arr) - 2, -1, -1):
+        if i == 0:
+            arr[i+1] = arr[i]
+            arr[i] = element_to_move
+        elif element_to_move < arr[i]:
+            arr[i + 1] = arr[i]
+        elif element_to_move > arr[i]:
+            arr[i + 1] = element_to_move
+            break
+
+    print(arr)
+
+
+if __name__ == "__main__":
+    sort_the_array([1, 2, 3, 6, 8, 15, 20, 14])
+    sort_the_array([3, 5, 7, 8, 9, 12, 15, 16, 19, 25, 30, 1])
