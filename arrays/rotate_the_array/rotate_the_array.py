ce4f17ecd09a151114bf70deb4edc0de6b9023a5
Krupa-Ghetia <krips2407@gmail.com>
2022-07-29 05:29:22 +0530

    refactor  rotate array

--- arrays/rotate_the_array/rotate_the_array.py
+++ arrays/rotate_the_array/rotate_the_array.py
@@ -1,21 +1,14 @@
 def rotate_array(arr, k):
-    if k >= len(arr):
-        print("Rotation steps should be less than array length")
-        return
 
     arr_len = len(arr)
-    temp_subarr = arr[arr_len - k: arr_len]
-    subarray_idx = k - 1
+    pos_arr = []
 
-    for i in range(arr_len - 1, -1, -1):
-        elem_idx_to_move = i - k
+    for i in range(0, arr_len):
+        shift_pos = (i+k)%arr_len
+        pos_arr.append((shift_pos, arr[i]))
 
-        if elem_idx_to_move < 0:
-            arr[i] = temp_subarr[subarray_idx]
-            subarray_idx -= 1
-            continue
-
-        arr[i] = arr[elem_idx_to_move]
+    for x in pos_arr:
+        arr[x[0]] = x[1]
 
     print(arr)
 
@@ -23,5 +16,5 @@ def rotate_array(arr, k):
 if __name__ == "__main__":
     rotate_array([1, 2, 3, 4, 5, 6, 7], 3)
     rotate_array([1, 2, 3, 4, 5, 6, 7], 1)
-    rotate_array([1, 2, 3, 4], 4)
+    rotate_array([1, 2, 3, 4], 2)
     rotate_array([1, 2, 3], 5)
