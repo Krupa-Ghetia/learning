57d997ebb69451b1dd7ed4432c2459e750eeed19
Krupa-Ghetia <krips2407@gmail.com>
2022-07-29 09:12:16 +0530

    refactor spiral matrix

--- arrays/spiral_matrix/spiral_matrix.py
+++ arrays/spiral_matrix/spiral_matrix.py
@@ -3,36 +3,23 @@ def print_matrix_spirally(mat, rows, cols):
     right = cols - 1
     top = 0
     bottom = rows - 1
-    visited_elems = 0
-    total_elems = rows * cols
 
-    while visited_elems < total_elems:
-        j = left
-        while j <= right and visited_elems <total_elems:
+    while left <= right and top <= bottom:
+
+        for j in range(left, right+1):
             print(mat[top][j], end=" ")
-            j += 1
-            visited_elems += 1
         top = top + 1
 
-        j = top
-        while j <= bottom and visited_elems <total_elems:
+        for j in range(top, bottom+1):
             print(mat[j][right], end=" ")
-            j += 1
-            visited_elems += 1
         right = right - 1
 
-        j = right
-        while j >= left and visited_elems < total_elems:
+        for j in range(right, left-1, -1):
             print(mat[bottom][j], end=" ")
-            j -= 1
-            visited_elems += 1
         bottom = bottom - 1
 
-        j = bottom
-        while j >= top and visited_elems < total_elems:
+        for j in range(bottom, top-1, -1):
             print(mat[j][left], end=" ")
-            j -= 1
-            visited_elems += 1
         left = left + 1
     print("")
 
@@ -40,5 +27,3 @@ def print_matrix_spirally(mat, rows, cols):
 if __name__ == "__main__":
     print_matrix_spirally([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 3, 3)
     print_matrix_spirally([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]], 4, 4)
-    print_matrix_spirally([[1,2,3],[4,5,6],[7,8,9],[10,11,12]], 4, 3)
-    print_matrix_spirally([[1,2,3,4],[5,6,7,8],[9,10,11,12]], 3, 4)
