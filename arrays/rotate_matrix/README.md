b175bdbb8722e946efc97b1bb4fc0f8a17ed8fc0
Krupa-Ghetia <krips2407@gmail.com>
2022-07-31 14:14:13 +0530

    solve rotate matrix clockwise by 90 degrees

---
+++ arrays/rotate_matrix/README.md
@@ -0,0 +1,11 @@
+ Rotate a matrix clockwise by 90 degrees.
+ 
+Example:
+```buildoutcfg
+mat = [[1,2,3],[4,5,6],[7,8,9]]
+```
+
+Output:
+```buildoutcfg
+rotated_mat = [[7,4,1],[8,5,2],[9,6,3]]
+```
---
+++ arrays/rotate_matrix/rotate_matrix.py
@@ -0,0 +1,33 @@
+import math
+
+
+def rotate_matrix(mat):
+    mat = transpose_matrtix(mat)
+    mat = reverse_rows(mat)
+
+    print(mat)
+
+
+def transpose_matrtix(mat):
+
+    for i in range(0, len(mat)):
+        for j in range(i, len(mat)):
+            mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+
+    return mat
+
+
+def reverse_rows(mat):
+
+    mat_len = len(mat)
+    for i in range(0, mat_len):
+        for j in range(0, math.floor(mat_len/2)):
+            mat[i][j], mat[i][mat_len - j - 1] = mat[i][mat_len - j - 1], mat[i][j]
+
+    return mat
+
+
+if __name__ == "__main__":
+    rotate_matrix([[1,2,3],[4,5,6],[7,8,9]])
+    rotate_matrix([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+
