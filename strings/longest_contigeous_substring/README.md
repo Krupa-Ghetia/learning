23e10ebbd52cd3c45d84267d24f5ae2b6a5d4098
Krupa-Ghetia <krips2407@gmail.com>
2022-08-05 16:13:44 +0530

    solve longest contigeous substring

---
+++ strings/longest_contigeous_substring/README.md
@@ -0,0 +1,9 @@
+Given a binary string of 0's and 1's, you can swap one 0 with one 1. Find the length of the longest contigeous substring of 1's possible.  
+
+Example:
+```buildoutcfg
+Input:
+    input_str = 111011101
+Output:
+    size = 7
+```
---
+++ strings/longest_contigeous_substring/longest_contigeous_substring.py
@@ -0,0 +1,40 @@
+def longest_contigeous_substring(input_str):
+    left = []
+    right = []
+    max_len = 0
+    total_ones = 0
+
+    cons_count = 0
+    for val in input_str:
+        if val == '1':
+            cons_count += 1
+            total_ones += 1
+            left.append(cons_count)
+        else:
+            cons_count = 0
+            left.append(cons_count)
+
+    cons_count = 0
+    for val in input_str[::-1]:
+        if val == '1':
+            cons_count += 1
+            right.insert(0, cons_count)
+        else:
+            cons_count = 0
+            right.insert(0, cons_count)
+
+    for i in range(0, len(input_str)-1):
+        if input_str[i] == '1':
+            max_len = max(max_len, (left[i] + right[i] - 1))
+        elif (left[i-1] + right[i+1] + 1) <= total_ones:
+            max_len = max(max_len, (left[i-1] + right[i+1] + 1))
+        else:
+            max_len = max(max_len, (left[i-1] + right[i+1]))
+
+    print(max_len)
+
+
+if __name__ == "__main__":
+    longest_contigeous_substring("111011101")
+    longest_contigeous_substring("1111011110")
+    longest_contigeous_substring("11111")
