5b2164688f6419fa7306a158f290fefdee349bac
Krupa-Ghetia <krips2407@gmail.com>
2022-08-05 17:26:43 +0530

    refactor longest contigeous substring

--- strings/longest_contigeous_substring/longest_contigeous_substring.py
+++ strings/longest_contigeous_substring/longest_contigeous_substring.py
@@ -24,12 +24,20 @@ def longest_contigeous_substring(input_str):
             right.insert(0, cons_count)
 
     for i in range(0, len(input_str)-1):
-        if input_str[i] == '1':
-            max_len = max(max_len, (left[i] + right[i] - 1))
-        elif (left[i-1] + right[i+1] + 1) <= total_ones:
-            max_len = max(max_len, (left[i-1] + right[i+1] + 1))
+        if i == 0:
+            if input_str[i] == '1':
+                max_len = max(max_len, (left[i] + right[i] - 1))
+            elif (right[i + 1] + 1) <= total_ones:
+                max_len = max(max_len, (right[i + 1] + 1))
+            else:
+                max_len = max(max_len, (right[i + 1]))
         else:
-            max_len = max(max_len, (left[i-1] + right[i+1]))
+            if input_str[i] == '1':
+                max_len = max(max_len, (left[i] + right[i] - 1))
+            elif (left[i-1] + right[i+1] + 1) <= total_ones:
+                max_len = max(max_len, (left[i-1] + right[i+1] + 1))
+            else:
+                max_len = max(max_len, (left[i-1] + right[i+1]))
 
     print(max_len)
 
@@ -38,3 +46,4 @@ if __name__ == "__main__":
     longest_contigeous_substring("111011101")
     longest_contigeous_substring("1111011110")
     longest_contigeous_substring("11111")
+    longest_contigeous_substring("011110001001")
