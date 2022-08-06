398164a374ee57b4f8f72db4b4e7f4bdae52a2f0
Krupa-Ghetia <krips2407@gmail.com>
2022-08-06 17:07:57 +0530

    refactor array intersection

--- hashing/array_intersection/array_intersection.py
+++ hashing/array_intersection/array_intersection.py
@@ -1,15 +1,17 @@
 def array_intersection(arr1, arr2):
-    arr1_map = dict()
+    array_map = dict()
     for x in arr1:
-        arr1_map[x] = 1
+        count = array_map.get(x, 0)
+        array_map[x] = count + 1
 
-    intersection = set()
+    intersection = list()
     for x in arr2:
-        if arr1_map.get(x, 0):
-            intersection.add(x)
+        if array_map.get(x, 0):
+            array_map[x] -= 1
+            intersection.append(x)
 
     print(intersection)
 
 
 if __name__ == "__main__":
-    array_intersection([1, 2, 3, 5, 4, 6, 8], [2, 7, 9, 3, 3])
+    array_intersection([1, 2, 3, 5, 4, 6, 8, 3], [2, 7, 9, 3, 3, 3])
