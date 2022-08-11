7b869e5441709b3aba8d1d95a385ec509383a07e
Krupa-Ghetia <krips2407@gmail.com>
2022-08-11 17:34:06 +0530

    refactor perfect cards

--- hashing/perfect_cards/perfect_cards.py
+++ hashing/perfect_cards/perfect_cards.py
@@ -6,7 +6,7 @@ def perfect_cards(A):
         card_hash[A[i]] = hash_value + 1
 
     keys = list(card_hash.keys())
-    if len(keys) > 2:
+    if len(keys) != 2:
         print("LOSE")
     elif card_hash.get(keys[0]) != card_hash.get(keys[1]):
         print("LOSE")
@@ -18,3 +18,4 @@ if __name__ == "__main__":
     perfect_cards([1, 1, 2, 2, 2])
     perfect_cards([1, 2])
     perfect_cards([1, 1, 2, 2, 3])
+    perfect_cards([6, 6])
