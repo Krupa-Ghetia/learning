5f342260ebb71c6a2c5c9321213222c8fe2922ef
Krupa-Ghetia <krips2407@gmail.com>
2022-08-20 01:11:26 +0530

    refactor verify AP

--- arrays/verify_AP/verify_AP.py
+++ arrays/verify_AP/verify_AP.py
@@ -1,12 +1,17 @@
 def verify_AP(arr):
-    diff = abs(arr[0] - arr[1])
     first_elem = min(arr)
+    second_elem = max(arr)
     elem_hash = dict()
 
     for el in arr:
+        if first_elem < el < second_elem:
+            second_elem = el
+
         elem_hash[el] = 0
 
-    for i in range(0, len(arr) - 1):
+    diff = abs(first_elem - second_elem)
+
+    for i in range(0, len(arr)):
         if (first_elem + (i * diff)) not in elem_hash:
             print(False)
             return
@@ -16,3 +21,5 @@ def verify_AP(arr):
 if __name__ == "__main__":
     verify_AP([3, 1, 5])
     verify_AP([3, 5, 2, 8])
+    verify_AP([2, 4, 1])
+    verify_AP([-39, -39])
