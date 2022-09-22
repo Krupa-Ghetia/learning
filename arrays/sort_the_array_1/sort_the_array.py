25465556ae47f5092167626ab17e68b7ffecabe4
Krupa-Ghetia <krips2407@gmail.com>
2022-09-22 21:34:59 +0530

    refactor sort the array

--- arrays/sort_the_array_1/sort_the_array.py
+++ arrays/sort_the_array_1/sort_the_array.py
@@ -3,8 +3,11 @@ def sort_the_array(arr):
 
     for i in range(len(arr) - 2, -1, -1):
         if i == 0:
-            arr[i+1] = arr[i]
-            arr[i] = element_to_move
+            if element_to_move < arr[i]:
+                arr[i+1] = arr[i]
+                arr[i] = element_to_move
+            else:
+                arr[i + 1] = element_to_move
         elif element_to_move < arr[i]:
             arr[i + 1] = arr[i]
         elif element_to_move > arr[i]:
@@ -17,3 +20,4 @@ def sort_the_array(arr):
 if __name__ == "__main__":
     sort_the_array([1, 2, 3, 6, 8, 15, 20, 14])
     sort_the_array([3, 5, 7, 8, 9, 12, 15, 16, 19, 25, 30, 1])
+    sort_the_array([1, 3, 6, 8, 15, 20, 2])
